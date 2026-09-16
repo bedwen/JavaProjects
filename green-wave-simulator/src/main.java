7c798e456742f95b8230ac7c9802b1a6549ced44
bedwen <219533298+bedwen@users.noreply.github.com>
2026-09-16 22:34:41 +0300

    trying to make green wave simulator

---
+++ green-wave-simulator/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    static void main(String[] args) {
+        //green wave simulator
+        //take car speed
+        //take traffic light location
+        //calculate the distance between the car and the traffic light
+        //and calculate the optimize car's speed for green light
+        //GPS data from other vehicles might be needed for traffic density,
+        //but for this project, such data would be overkill
+    }
+}
